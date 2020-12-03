8717faabc39c147cc8f1fd8ad16ea95a5bb651b2
Daniel Carter <62474051+carterd888@users.noreply.github.com>
2020-12-03 13:16:40 +0000

    added post for new orders

--- cyf-ecommerce-api/server.js
+++ cyf-ecommerce-api/server.js
@@ -71,6 +71,7 @@ app.post("/customers", function (req, res) {
       } else {
         const query =
           "INSERT INTO customers (name, address, city, country) VALUES ($1, $2, $3, $4)";
+
         pool
           .query(query, [
             newCustomerName,
@@ -97,17 +98,39 @@ app.post("/products", function (req, res) {
       if (result.rows.length === 0) {
         return res.status(400).send("This supplier ID does not exist.");
       } else if (!Number.isInteger(newProductPrice) || newProductPrice <= 0) {
-  return res
-    .status(400)
-    .send("The price should be a positive integer.");
-} else {
-
+        return res
+          .status(400)
+          .send("The price should be a positive integer.");
+      } else {
         const query =
           "INSERT INTO products(product_name, unit_price, supplier_id) VALUES ($1, $2, $3)";
+
         pool
           .query(query, [newProductName, newProductPrice, newProductSupplier])
           .then(() => res.send("Product created!"))
           .catch((e) => console.error(e));
+      }
+    });
+});
+
+app.post("/customers/:customerId/orders", function (req, res) {
+  const newOrderDate = req.body.date;
+  const newOrderReference = req.body.reference;
+
+  const customerId = req.params.customerId;
+
+  pool.query("SELECT * FROM customers WHERE id = $1", [customerId])
+.then((result) => {
+        if (result.rows.length === 0) {
+        return res.status(400).send("This customer ID does not exist.");
+      } else {
+        const query =
+          "INSERT INTO orders (order_date, order_reference, customer_id) VALUES ($1, $2, $3)";
+
+  pool
+    .query(query, [newOrderDate, newOrderReference, customerId])
+    .then(() => res.send("Order created!"))
+    .catch((e) => console.error(e));
       }
     });
 });
