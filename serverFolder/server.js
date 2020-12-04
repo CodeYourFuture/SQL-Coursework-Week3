dd67c4c615172b30e6f40d81435758a2c2c2e9df
davibaweja <davibaweja@gmail.com>
2020-12-04 16:40:17 +0000

    Added the /product end point
    This end point will return all products is a specific product name is not passed in the URL.

--- serverFolder/server.js
+++ serverFolder/server.js
@@ -36,16 +36,33 @@ app.get("/suppliers",function(req,res){
   .catch((e)=>console.log(e));
 });
 
-
-
 app.get("/products",function(req,res){
+  
+  let name=req.query.name;
+  console.log(name);
+  if(typeof (name)=="undefined")
+  {
+    console.log("Name is undefined")
+     const Query="select product_name, supplier_name  from products inner join suppliers on suppliers.id=products.supplier_id"; 
+     pool.query(Query)
+     .then((result)=>res.send(result.rows))
+     .catch((e)=>console.log(e));
+  }
+  else{
+    const Query="select product_name, supplier_name  from products inner join suppliers on suppliers.id=products.supplier_id where product_name=$1";
+    pool.query(Query,[name])
+    .then((result)=>res.send(result.rows))
+    .catch((e)=>console.log(e));
+  }
+  
+   
 
-  const Query="select product_name, supplier_name  from products inner join suppliers on suppliers.id=products.supplier_id";
-  pool.query(Query)
-  .then((result)=>res.send(result.rows))
-  .catch((e)=>console.log(e));
 });
+
+
+
 
 app.listen(3000,function(){
   console.log("Listening at port 3000");
 });
+
