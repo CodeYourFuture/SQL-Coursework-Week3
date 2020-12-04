6bf6c7ae831cd7651c7794ce4595cf17a1d3d8f2
Ade <62720742+Ade0211@users.noreply.github.com>
2020-12-04 19:30:36 +0000

    week 3 done

---
+++ .github/test.sql
@@ -0,0 +1,21 @@
+SELECT order_reference, order_date, product_name, unit_price,supplier_name, quantity 
+FROM customers 
+INNER JOIN orders ON orders.customer_id = customers.id 
+INNER JOIN order_items ON order_items.order_id=orders.id
+INNER JOIN products ON order_items.product_id =products.id 
+INNER JOIN suppliers ON products.supplier_id = suppliers.id 
+WHERE customer_id =1;
+
+SELECT order_reference, order_date, product_name, unit_price,supplier_name, quantity 
+FROM  orders
+INNER JOIN order_items ON order_items.order_id=orders.id
+INNER JOIN products ON order_items.product_id =products.id 
+INNER JOIN suppliers ON products.supplier_id = suppliers.id 
+WHERE customer_id =1;
+
+SELECT order_reference, order_date, product_name, unit_price,supplier_name, quantity 
+FROM customers 
+INNER JOIN orders ON orders.customer_id = customers.id 
+INNER JOIN products ON products.supplier_id = suppliers.id 
+INNER JOIN order_items ON order_items.product_id =products.id 
+WHERE customer_id = 1;
