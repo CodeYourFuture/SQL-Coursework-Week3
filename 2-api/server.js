0234d5bf578427aa82d25be2b53dd58c62be9cd8
Michael <antenehnigussie1@yahoo.com>
2021-05-24 10:05:16 +0100

    Added references to routers. Added routers for "/costomers", "/suppliers" and "/products" routes.

--- 2-api/server.js
+++ 2-api/server.js
@@ -6,3 +6,11 @@ app.listen(5000, function () {
   console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
 });
 
+const customers = require("./customers/customersRouter");
+const suppliers = require("./suppliers/suppliersRouter");
+const products = require("./products/productsRouter");
+
+app.use("/customers", customers);
+app.use("/suppliers", suppliers);
+app.use("/products", products);
+
