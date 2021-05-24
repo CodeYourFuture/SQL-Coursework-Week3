8d7c9ffb9332bf5c02210c7f305a86ac75573897
Michael <antenehnigussie1@yahoo.com>
2021-05-24 09:34:18 +0100

    Moved db connection 'Pool' to a "config" file.

--- 2-api/server.js
+++ 2-api/server.js
@@ -6,11 +6,3 @@ app.listen(5000, function () {
   console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
 });
 
-const { Pool } = require("pg");
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "cyf_ecommerce",
-  password: "",
-});
