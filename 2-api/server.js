a44348fe94a23df32f24b30222c3f498ff7317f2
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-07 23:57:32 +0000

    feat: create server

---
+++ 2-api/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+app.use(express.json());
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "",
+  port: 5432,
+});
+
+app.get("/customers", function (req, res) {
+  pool.query("SELECT * FROM customers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
