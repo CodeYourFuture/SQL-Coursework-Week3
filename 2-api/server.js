1298c605a4c4454530159b6f2d83e7dc3c13eb93
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-08 00:12:59 +0000

    feat: read lists with endpoints

--- 2-api/server.js
+++ 2-api/server.js
@@ -17,6 +17,25 @@ app.get("/customers", function (req, res) {
   });
 });
 
+app.get("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+  pool.query(
+    "SELECT * FROM customers WHERE customers.id=$1",
+    [customerId],
+    (error, result) => {
+      res.json(result.rows);
+    }
+  );
+});
+
+app.get("/suppliers", function (req, res) {
+  pool.query("SELECT * FROM suppliers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+
+
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
