0d1ecab0abd7d70d0f9f35534d2bb70ba3a0c844
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-09 13:48:02 +0000

    feat: add get products

--- 2-api/server.js
+++ 2-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const { Pool } = require("pg");
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const pool = new Pool({
@@ -11,31 +12,46 @@ const pool = new Pool({
   port: 5432,
 });
 
+app.get("/", (req, res) => {
+  res.send("It's ready to GET");
+}); 
 app.get("/customers", function (req, res) {
-  pool.query("SELECT * FROM customers", (error, result) => {
-    res.json(result.rows);
+  pool
+    .query("SELECT * FROM customers", (error, result) => {
+      res.json(result.rows);
   });
 });
 
 app.get("/customers/:customerId", function (req, res) {
   const customerId = req.params.customerId;
-  pool.query(
-    "SELECT * FROM customers WHERE customers.id=$1",
-    [customerId],
-    (error, result) => {
-      res.json(result.rows);
+  pool
+    .query("SELECT * FROM customers WHERE customers.id=$1",
+      [customerId],
+      (error, result) => {
+        res.json(result.rows);
     }
   );
 });
 
 app.get("/suppliers", function (req, res) {
-  pool.query("SELECT * FROM suppliers", (error, result) => {
-    res.json(result.rows);
-  });
+  pool
+    .query("SELECT * FROM suppliers", (error, result) => {
+      res.json(result.rows);
+    });
+});
+
+app.get("/products", (req, res) => {
+  pool
+    .query(
+      "SELECT p.product_name, pa.unit_price, s.supplier_name FROM products p JOIN product_availability pa ON p.id = pa.prod_id JOIN suppliers s ON pa.supp_id = s.id",
+      (error, result) => {
+        res.json(result.rows);
+      })
 });
 
 
 
-app.listen(3000, function () {
-  console.log("Server is listening on port 3000. Ready to accept requests!");
+
+app.listen(PORT, function () {
+  console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
 });
