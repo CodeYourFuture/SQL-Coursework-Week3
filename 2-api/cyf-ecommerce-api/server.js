ccb060bcd059060610b01cc5834c5c0876ef6516
maham511 <maha.malik511@gmail.com>
2021-12-13 12:49:47 +0000

    post request on /Availability works. Still need to throw error when prod or supp id don't exist in db

--- 2-api/cyf-ecommerce-api/server.js
+++ 2-api/cyf-ecommerce-api/server.js
@@ -83,29 +83,131 @@ app.post("/customers", function (req, res) {
 
 // -----------------
 
-app.post("/products", function (req, res) {
-    const newProductName = req.body.name;
+// app.post("/availability", function (req, res) {
+//   const productId = req.body.prodId;
+//   const suppId = req.body.suppId;
+//   const price = req.body.price;
 
-  if (!newProductName) {
-    return res.status(400).send("Please enter product name");
+//   if (!Number.isInteger(price) || price <= 0) {
+//     return res.status(400).send("Please enter a positive price");
+//   }
+
+//   pool.query(
+//     "SELECT products.id, suppliers.id FROM products, suppliers WHERE p.id=$1 AND s.id=$2", [productId, suppId])
+//     .then(
+//       (result) => {
+//         if (result.rows.length < 1) {
+//           return res.status(400).send("Product doesn't exist!");
+//         } else {
+//           const query =
+//             "INSERT INTO product_availability (prod_id, supp_id, unit_price) VALUES ($1, $2, $3)";
+//           pool
+//             .query(query, [productId, suppId, price])
+//             .then(() => res.send("availability added"))
+//             .catch((e) => console.error(e));
+//         }
+//       }
+//     );
+// });
+
+
+app.post("/availability", (req, res) => {
+  const productId = req.body.prodId;
+  const newPrice = req.body.price;
+  const supplierId = req.body.supplierId;
+  if (newPrice <= 0) {
+    return res.status(404).send("Invalid pricing option");
   }
   pool
-    .query("SELECT product_name FROM products WHERE product_name=$1", [
-      newProductName,
-    ])
+    .query(
+      "SELECT products.id, suppliers.id FROM products INNER JOIN product_availability ON products.id = product_availability.prod_id INNER JOIN suppliers on suppliers.id = product_availability.supp_id WHERE products.id=$1 OR suppliers.id=$2",
+      [productId, supplierId]
+    )
     .then((result) => {
-      if (result.rows.length > 0) {
-        return res.status(400).send("PRODUCT already exists!");
+      if (result.rowCount === 0) {
+        return res.status(400).send("Product or Supplier doesn't exist");
       } else {
-        const query = "INSERT INTO products (product_name) VALUES ($1)";
-        pool
-          .query(query, [newProductName])
-          .then(() => res.send("product added"))
-          .catch((e) => console.error(e));
+        pool.query(
+          "INSERT INTO product_availability (prod_id, unit_price, supp_id) VALUES ($1, $2, $3)",
+          [productId, newPrice, supplierId],
+          (err, result) => {
+            if (!err) {
+              res
+                .status(200)
+                .send("New product availability added")
+                .json(result.rows);
+            } else {
+              res.status(400);
+              console.error(err);
+            }
+          }
+        );
       }
     });
 });
+// ----------------------------------
+//insert prod_id,unit_price & supp_id -Donee
+//Check that the price is a positive integer -Done
+//Check both the productID and supplier ID's exist in the database,
+// otherwise return an error.
+//UNSURE OF WHAT NEEDS RETURNING
+//Adding both statements in pool.query didn't work ----  FROM product_availability WHERE prod_id=$1 OR supp_id = $2"
 
+// app.post("/availability", function (req, res) {
+//   const newProductId = req.body.prodId;
+//   const newSuppId = req.body.suppId;
+//   const newPrice = req.body.price;
+
+
+//   if (!Number.isInteger(newPrice) || newPrice <= 0) {
+//     return res.status(400).send("Please enter a positive price");
+//   }
+
+//   pool
+//     .query(
+//       "SELECT * FROM products WHERE id=$1", [newProductId]
+//     )
+//     .then((result) => {
+//       if (result.rows.length < 1) {
+//         return res.status(400).send("Product doesn't exist!");
+//       } else {
+//         const query =
+//           "INSERT INTO product_availability (prod_id, supp_id, unit_price) VALUES ($1, $2, $3)";
+//         pool
+//           .query(query, [newProductId, newSuppId, newPrice])
+//           .then(() => res.send("availability added"))
+//           .catch((e) => console.error(e));
+//       }
+//     });
+// });
+
+// ---------------
+// copy of availability post req
+
+// app.post("/availability", function (req, res) {
+//   const newProductId = req.body.prodId;
+//   const newSuppId = req.body.suppId;
+//   const newPrice = req.body.price;
+
+//   if (!Number.isInteger(newPrice) || newPrice <= 0) {
+//     return res.status(400).send("Please enter a positive price");
+//   }
+
+//   pool
+//     .query("SELECT * FROM products WHERE id=$1", [newProductId])
+//     .then((result) => {
+//       if (result.rows.length < 1) {
+//         return res.status(400).send("Product doesn't exist!");
+//       } else {
+//         const query =
+//           "INSERT INTO product_availability (prod_id, supp_id, unit_price) VALUES ($1, $2, $3)";
+//         pool
+//           .query(query, [newProductId, newSuppId, newPrice])
+//           .then(() => res.send("availability added"))
+//           .catch((e) => console.error(e));
+//       }
+//     });
+// });
 
 
 
