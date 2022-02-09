2aea1ad5efe3a035eb723d2b5dbce76eb41937e1
haseeb3927 <haseeb3927@gmail.com>
2022-02-09 15:14:38 +0000

    All Challenges completed

--- 2-api/cyf-ecommerce-api/server.js
+++ 2-api/cyf-ecommerce-api/server.js
@@ -70,6 +70,30 @@ app.get("/suppliers", function (req, res) {
       res.status(500).json(error);
     });
 });
+app.get("/customers/:customerId/orders", function (req, res) {
+  let customerId = req.params.customerId;
+  let query = `SELECT customers.name,orders.order_reference, orders.order_date, products.product_name,product_availability.unit_price, suppliers.supplier_name, order_items.quantity
+   FROM customers
+   JOIN orders
+   ON orders.id = orders.customer_id
+   JOIN order_items 
+   ON order_items.order_id = orders.id
+   JOIN products
+   ON products.id = order_items.product_id
+   JOIN suppliers 
+   ON suppliers.id = order_items.supplier_id
+   JOIN product_availability 
+   ON product_availability.supp_id = order_items.product_id
+   WHERE customers.id = $1
+   ORDER BY orders.order_date `;
+  pool
+    .query(query, [customerId])
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
 
 // ADVANCED LEVEL
 app.get("/products", function (req, res) {
@@ -129,6 +153,49 @@ app.post("/products", function (req, res) {
     });
 });
 
+//  ADD PRODUCT-AVAILABILITY
+app.post("/availability", function (req, res) {
+  let productId = req.body.prod_id;
+  let productPrice = req.body.unit_price;
+  let supplierId = req.body.supp_id;
+
+  if (!Number.isInteger(productPrice) || productPrice <= 0) {
+    return res.status(400).send({ msg: `Please Enter a a valid Price.` });
+  }
+  if (!productId || !supplierId) {
+    return res.status(400).send("Product Id or Supplier Id missing");
+  }
+
+  let query1 = `SELECT * product_availability  WHERE prod_id = $1`; //Check if product_ID exist in data
+  let query2 = `SELECT * product_availability  WHERE supp_id = $1`; // Check if supplier_ID exist in data
+  //   let params = [productId];
+  pool.query(query1, [productId]).then((result) => {
+    if (result.rows.length === 0) {
+      res.send({ msg: `Product:${productId} Does not exist ! ` });
+    } else if (result.rows.length > 0) {
+      pool.query(query2, [supplierId]).then((result) => {
+        if (result.rows.length === 0) {
+          res.send({
+            msg: `Supplier:${supplierId} Does not exist !`,
+          });
+        }
+      });
+    } else {
+      let query = `INSERT INTO product_availability (prod_id,unit_price,supp_id ) VALUES($1,$2,$3)`;
+      let params = [productId, productPrice, supplierId];
+      pool
+        .query(query, params)
+        .then(() =>
+          res.send({ msg: `Product:${productId} added successfully ` })
+        )
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json(error);
+        });
+    }
+  });
+});
+
 // GET CUSTOMER WITH AN ID
 app.get("/customers/:customerId", function (req, res) {
   let customerId = req.params.customerId;
@@ -149,6 +216,122 @@ app.get("/customers/:customerId", function (req, res) {
       res.status(500).json(error);
     });
 });
+// CREATE A NEW ORDER
+app.post("/customers/:customerId/orders", function (req, res) {
+  let customerId = req.params.customerId;
+  let orderRef = req.body.order_reference;
+  let orderDate = req.body.order_date;
+
+  let query = `SELECT * FROM customers WHERE id = $1`;
+  pool.query(query, [customerId]).then((result) => {
+    if (result.rows.length == 0) {
+      return res
+        .status(404)
+        .send({ msg: `Customer:${customerId} doesn't exist` });
+    } else {
+      let query = `INSERT INTO orders (order_date,order_reference,customer_id ) VALUES ($1,$2,$3)`;
+      let params = [orderDate, orderRef, customerId];
+      pool
+        .query(query, params)
+        .then(() =>
+          res.status(404).send({ msg: `Order:${orderRef} added successfully` })
+        )
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json(error);
+        });
+    }
+  });
+});
+
+// UPDATE A CUSTOMER'S INFO
+
+app.put("/customers/:customerId", function (req, res) {
+  let customerId = req.params.customerId;
+  const newName = req.body.name;
+  const newAddress = req.body.address;
+  const newCity = req.body.city;
+  const newCountry = req.body.country;
+
+  // Checking if the customer with Id entered exist or not
+  pool
+    .query(`SELECT * FROM customers WHERE id = $1`, [customerId])
+    .then((result) => {
+      if (result.rows.length == 0) {
+        return res
+          .status(404)
+          .send({ msg: `Customer ${customerId} doesn't exist` });
+      }
+    });
+
+  // First we select the customer then we can update the changes else we will can return the old info
+  pool
+    .query(`SELECT * FROM customers WHERE id = $1`, [customerId])
+    .then((result) => {
+      let originalCustomer = result.rows[0];
+      let updateQuery = `UPDATE customers
+        SET name = $2, address = $3, city = $4, country = $5
+        WHERE id = $1`;
+      let params = [
+        customerId,
+        newName || originalCustomer.name,
+        newAddress || originalCustomer.address,
+        newCity || originalCustomer.city,
+        newCountry || originalCustomer.country,
+      ];
+
+      pool
+        .query(updateQuery, params)
+        .then(() => res.send(`Customer ${customerId} updated!`))
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json(error);
+        });
+    });
+});
+
+// DELETE ORDER
+
+app.delete("/orders/:orderId", function (req, res) {
+  let orderId = req.params.orderId;
+  // First we have to delete all the items in that order
+  let itemsDelQuery = `DELETE FROM order_items where order_id = $1 `;
+  let orderDelQuery = `DELETE FROM orders where id = $1`;
+  pool
+    .query(itemsDelQuery, [orderId])
+    .then(() => pool.query(orderDelQuery, [orderId]))
+    .then(() => res.send({ msg: `Order: ${orderId} deleted successfully` }))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.delete("/customers/:customerId", function (req, res) {
+  let customerId = req.params.customerId;
+
+  let query = `DELETE FROM customers where id = $1`;
+
+  pool
+    .query(`SELECT FROM orders WHERE customer_id = $1`, [customerId])
+    .then((result) => {
+      if (result.rows.length >= 1) {
+        return res.status(400).send({
+          msg: `Customer: ${customerId} can not be deleted because they have pending orders !`,
+        });
+      }
+      pool
+        .query(query, [customerId])
+        .then(() =>
+          res.send({ msg: `Customer: ${customerId} deleted successfully ` })
+        )
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json(error);
+        });
+    });
+});
+
 app.listen(3001, function () {
   console.log("Server is listening on port 3001. Ready to accept requests!");
 });
