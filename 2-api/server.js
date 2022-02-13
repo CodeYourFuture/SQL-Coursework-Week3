25ad255e1cc68e876d8a711058d89eb8ec2c956f
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2022-02-13 20:29:05 +0000

    Fixed previous get products query and completed several tasks
    added:
    - fixed /products query
    - added new param end point for customers/:customerId
    - added post functionality for /customers

--- 2-api/server.js
+++ 2-api/server.js
@@ -2,12 +2,30 @@ const { response } = require("express");
 const express = require("express");
 const postgres = require("pg");
 app = express();
+app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT;
 const pool = new postgres.Pool({
   connectionString: process.env.PG_CONNECT,
 });
+
+app.get("/customers/:customerId", (req, res) => {
+  pool.connect().then((client) => {
+    return client
+      .query(`SELECT * 
+      FROM customers AS c
+      WHERE c.id = $1`, [`${req.params.customerId}`])
+      .then((result) => {
+        client.release();
+        res.send(result.rows);
+      })
+      .catch(error => {
+        console.error(error);
+        res.send(error);
+      })
+  })
+})
 
 app.get("/customers", (req, res) => {
   pool.connect().then((client) => {
@@ -23,6 +41,25 @@ app.get("/customers", (req, res) => {
       });
   });
 });
+
+app.post("/customers", (req, res) => {
+  console.log(req.body);
+  pool.connect().then((client) => {
+    return client
+      .query(`
+      INSERT INTO customers (name, address, city, country)
+      VALUES($1, $2, $3, $4)
+      `, [`${req.body.name}`, `${req.body.address}`, `${req.body.city}`, `${req.body.country}`])
+      .then(() => {
+        client.release();
+        res.send(`Successfully added new customer.`)
+      })
+      .catch((error) => {
+        console.error(error);
+        res.send(error);
+      })
+  })
+})
 
 app.get("/suppliers", (req, res) => {
   pool.connect().then((client) => {
@@ -41,37 +78,49 @@ app.get("/suppliers", (req, res) => {
 
 app.get("/products", (req, res) => {
   pool.connect().then((client) => {
-    let thisQuery;
     if (req.query.name) {
-      (thisQuery = `
+      return client
+        .query(
+          `
                 SELECT p.product_name, pa.unit_price, s.supplier_name
                 FROM product_availability AS pa
                 JOIN products AS p
                 on p.id = pa.prod_id
                 JOIN suppliers AS s
                 ON pa.supp_id = s.id
-                WHERE p.product_name LIKE(%$1%)
-            `), [req.query.name];
-    }else{
-      thisQuery = `
+                WHERE LOWER(p.product_name) LIKE($1)
+            `,
+          [`%${req.query.name.toLowerCase()}%`]
+        )
+        .then((result) => {
+          client.release();
+          res.send(result.rows);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.send(error);
+        });
+    } else {
+      return client
+        .query(
+          `
                 SELECT p.product_name, pa.unit_price, s.supplier_name
                 FROM product_availability AS pa
                 JOIN products AS p
                 on p.id = pa.prod_id
                 JOIN suppliers AS s
                 ON pa.supp_id = s.id
-            `;
+            `
+        )
+        .then((result) => {
+          client.release();
+          res.send(result.rows);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.send(error);
+        });
     }
-    return client
-      .query(thisQuery)
-      .then((result) => {
-        client.release();
-        res.send(result.rows);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.send(error);
-      });
   });
 });
 
