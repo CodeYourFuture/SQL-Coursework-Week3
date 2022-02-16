68638eaaa93b79648f2c7b072763a67fdc322876
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2022-02-16 19:54:00 +0000

    stuck on deleting rows with foreign key constraints

--- 2-api/server.js
+++ 2-api/server.js
@@ -74,24 +74,92 @@ app.post("/customers", (req, res) => {
   });
 });
 
-//updates an existing customer with the specified id 
+//updates an existing customer with the specified id
 app.put("/customers/:customerId", (req, res) => {
   pool.connect().then((client) => {
     const c = req.body;
     const cId = req.params.customerId;
-    return client.query(
-      `
+    return client
+      .query(
+        `
         UPDATE customers
         SET name=$1, address=$2, city=$3, country=$4 
         WHERE id=$5
       `,
-      [c.name, c.address, c.city, c.country, cId]
-    ).then(() => {
-      res.send(`Successfully updated customer with the id of ${cId}`)
-    }).catch((error) => {
-      console.error(error);
-      res.status(400).send(error);
-    });
+        [c.name, c.address, c.city, c.country, cId]
+      )
+      .then(() => {
+        res.send(`Successfully updated customer with the id of ${cId}`);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(400).send(error);
+      });
+  });
+});
+
+/*
+  - Add a new DELETE endpoint `/customers/:customerId`
+  to delete an existing customer only if this customer
+  doesn't have orders.
+*/
+app.delete("/customers/:customerId", (req, res) => {
+  const cId = req.params.customerId;
+  pool.connect().then((client) => {
+    return client
+      .query(
+        `
+          SELECT COUNT(*) FROM orders WHERE customer_id=$1 
+        `,
+        [cId]
+      )
+      .then((result) => {
+        if (parseInt(result.rows[0].count > 0)) {
+          //has orders therefore can not delete
+          res.status(400).send(
+            `
+              Sorry, that customer has active orders
+              and therefore cannot be deleted
+            `
+          );
+        } else {
+          pool
+            .query(
+              `
+                DELETE oi
+                FROM order_items AS oi
+                JOIN orders AS o
+                ON oi.order_id = 0.id
+                WHERE o.customer_id = $1
+              `,
+              [cId]
+            )
+            .then(() => {
+              pool.query(
+                `
+                DELETE FROM orders WHERE customer_id=$1
+              `,
+                [cId]
+              );
+            })
+            .then(() => {
+              pool.query(
+                `
+                DELETE FROM customers WHERE id=$1
+              `,
+                [cId]
+              );
+            })
+            .then(() => {
+              client.release();
+              res.send(`Successfully deleted customer with ID: ${cId}`);
+            })
+            .catch((error) => {
+              console.error(error);
+              res.send(error);
+            });
+        }
+      });
   });
 });
 
@@ -106,10 +174,12 @@ app.post("/customers/:customerId/orders", (req, res) => {
         if (parseInt(result.rows[0].count) !== 1) {
           /* either somehow there are multiple customers with the 
           same ID or that customer doesn't exist */
-          res.status(400).send(`
-                                Something went wrong...
-                                Double check you have the correct customer ID
-                                `);
+          res.status(400).send(
+            `
+              Something went wrong...
+              Double check you have the correct customer ID
+            `
+          );
         } else {
           pool
             .query(
@@ -128,6 +198,35 @@ app.post("/customers/:customerId/orders", (req, res) => {
               res.send(error);
             });
         }
+      });
+  });
+});
+
+//deletes an order based on given id
+app.delete("/orders/:orderId", (req, res) => {
+  pool.connect().then((client) => {
+    const oId = req.params.orderId;
+    return client
+      .query(
+        `
+          DELETE FROM orders WHERE id=$1
+        `,
+        [oId]
+      )
+      .then(() => {
+        pool.query(
+          `
+            DELETE FROM order_items WHERE order_id = $1
+          `,
+          [oId]
+        );
+      })
+      .then(() => {
+        res.send(`Successfully deleted order with ID: ${oId}`);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.send(error);
       });
   });
 });
@@ -203,9 +302,9 @@ app.post("/availability", (req, res) => {
     return client
       .query(
         `
-      INSERT INTO product_availability(prod_id, unit_price, supp_id)
-      VALUES($1, $2, $3)
-      `,
+          INSERT INTO product_availability(prod_id, unit_price, supp_id)
+          VALUES($1, $2, $3)
+        `,
         [`${req.body.prod_id}`, `${req.body.unit_price}`, `${req.body.supp_id}`]
       )
       .then(() => {
