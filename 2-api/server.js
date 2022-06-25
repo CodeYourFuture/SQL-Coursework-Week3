e626ff9c40b16ebcf2240ec45d37028087cd2ee9
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-06-25 09:08:56 +0100

    POST new customer

--- 2-api/server.js
+++ 2-api/server.js
@@ -2,23 +2,24 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const { Pool } = require("pg");
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 // DATABASE
 const pool = new Pool({
-  user: process.env.SQL_USERNAME,
+  user: "cyf",
   host: "localhost",
   database: "cyf_ecommerce",
-  password: process.env.SQL_PASSWORD,
+  password: "cyfcyf",
   port: 5432,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Listen in http://localhost:${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listen in http://localhost:${PORT}`);
 });
 
-//Get
+// Get Products
 app.get("/products", (req, res) => {
   const { name } = req.query;
 
@@ -38,7 +39,7 @@ app.get("/products", (req, res) => {
     });
 });
 
-// Get all
+// Get all customers
 app.get("/customers", (req, res) => {
   let query = "SELECT * FROM customers;";
 
@@ -51,7 +52,7 @@ app.get("/customers", (req, res) => {
     });
 });
 
-// Get by id
+// Get customers by id
 app.get("/customers/:customerId", (req, res) => {
   const id = req.params.customerId;
 
@@ -61,5 +62,39 @@ app.get("/customers/:customerId", (req, res) => {
     .catch((error) => {
       console.log(error);
       res.status(500).json(error);
+    });
+});
+
+// POST new customer
+
+app.post("/customers", (req, res) => {
+  const newCustomerName = req.body.name;
+  const newCustomerAddress = req.body.address;
+  const newCustomerCity = req.body.city;
+  const newCustomerCountry = req.body.country;
+
+  pool
+    .query("SELECT * FROM customers WHERE name=$1", [newCustomerName])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return res
+          .status(400)
+          .send("A Customer with the same name already exists!");
+      } else {
+        const query =
+          "INSERT INTO customers (name, address, city, country) VALUES ($1, $2, $3, $4)";
+        pool
+          .query(query, [
+            newCustomerName,
+            newCustomerAddress,
+            newCustomerCity,
+            newCustomerCountry,
+          ])
+          .then(() => res.send("Customer created!"))
+          .catch((error) => {
+            console.error(error);
+            res.status(500).json(error);
+          });
+      }
     });
 });
