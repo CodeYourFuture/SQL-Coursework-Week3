8d6d65878bcd9165b151ad7e569168c984dc8ca4
lalehkoupa <lalehkoupa@gmail.com>
2022-06-26 22:20:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -255,6 +255,6 @@ app.get("/customers/:customerId/orders", (req, res) => {
 });
 
 const port = process.env.port || 3000;
-app.listen(3000, () => {
-  console.log(`server is listening on port 3000,ready accept requests!`);
+app.listen(port, () => {
+  console.log(`server is listening on port ${port},ready accept requests!`);
 });
