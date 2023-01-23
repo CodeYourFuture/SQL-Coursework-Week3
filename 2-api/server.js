52ef0d2399460c7f063f3bbc44a12d8acf783fab
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2023-01-23 11:41:06 +0200

    ZA2-christian-mfuke-SQL-Coursework-Week3
    getting all the products from database to  server and filter

---
+++ 2-api/server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "christianmfuke",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "Genese@1992",
+  port: 5432,
+});
+
+app.get("/customers", function (req, res) {
+  pool
+    .query("SELECT * FROM customers")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.get("/suppliers", function (req, res) {
+  pool
+    .query("SELECT * FROM suppliers")
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+// app.get("/products", function (req, res) {
+//   const sqlQuery =
+//     "SELECT products.product_name, product_availability.unit_price, suppliers.supplier_name FROM product_availability JOIN products ON product_availability.prod_id = products.id JOIN suppliers ON product_availability.supp_id = suppliers.id";
+//   pool
+//     .query(sqlQuery)
+//     .then((result) => res.json(result.rows))
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).json(error);
+//     });
+// });
+
+app.get("/products", function (req, res) {
+  let sqlQuery =
+    "SELECT products.product_name, product_availability.unit_price, suppliers.supplier_name FROM product_availability JOIN products ON product_availability.prod_id = products.id JOIN suppliers ON product_availability.supp_id = suppliers.id";
+  if (req.query.name) {
+    sqlQuery += " WHERE products.product_name = $1";
+  }
+  pool
+    .query(sqlQuery, [req.query.name])
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
