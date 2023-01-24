9a0acb79f6d137ba8949482bd4a2a1c0fb1c0b62
chandredewet <chandredewet@gmail.com>
2023-01-24 22:47:03 +0200

    completed first exercise

--- 2-api/cyf-ecommerce-api/server.js
+++ 2-api/cyf-ecommerce-api/server.js
@@ -37,19 +37,30 @@ app.get("/suppliers", function (req, res) {
     });
 });
 
+app.get("/", function (req, res) {
+  let searchQuery = req.query.search;
+  res.send("Hello World! You searched for " + searchQuery);
+});
+
+//Add a new GET endpoint `/products` to return all the product names along with their prices and supplier names.
+
+//week 3
 app.get("/products", function (req, res) {
+  let productQuery = req.query.name;
+  let query = `select products.product_name, product_availability.unit_price, suppliers.supplier_name from product_availability inner join products on products.id=product_availability.prod_id inner join suppliers on suppliers.id=product_availability.supp_id`;
+  let params = [];
+  if (productQuery) {
+    query = `select products.product_name, product_availability.unit_price, suppliers.supplier_name from product_availability inner join products on products.id=product_availability.prod_id inner join suppliers on suppliers.id=product_availability.supp_id where products.product_name = $1`;
+    params.push(productQuery);
+  }
   pool
-    .query(
-      "select products.product_name, product_availability.unit_price, suppliers.supplier_name from product_availability inner join products on products.id=product_availability.prod_id inner join suppliers on suppliers.id=product_availability.supp_id "
-    )
+    .query(query, params)
     .then((result) => res.json(result.rows))
     .catch((error) => {
       console.error(error);
       res.status(500).json(error);
     });
 });
-
-//Add a new GET endpoint `/products` to return all the product names along with their prices and supplier names.
 
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
