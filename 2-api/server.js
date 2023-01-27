ae0c39d4f12e141058d99d07a6b551c188718116
shafiekdavids <shafiekd@gmail.com>
2023-01-27 22:19:16 +0200

    Added more endpoints
    PUT and DELETE endpoints added

--- 2-api/server.js
+++ 2-api/server.js
@@ -96,6 +96,63 @@ app.post("/products", function (req, res) {
     });
 });
 
+// app.post("/availability", function (req, res) {
+//   const newProdId = req.body.prod_id;
+//   const newSuppId = req.body.supp_id;
+//   const newUnitPrice = req.body.unit_price;
+//   const query =
+//     "Insert into product_availability (prod_id, supp_id, unit_price) values ($1, $2, $3)";
+
+//   if (!Number.isInteger(newUnitPrice) || newUnitPrice <= 0) {
+//     return res
+//       .status(400)
+//       .send("The new unit price should be a positive integer");
+//   }
+
+//   pool.pool
+//     .query(query, [newProdId, newSuppId, newUnitPrice])
+//     .then(() => res.send("New Product Added"))
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).json(error);
+//     });
+// });
+
+// - Add a new PUT endpoint `/customers/:customerId` to update an existing customer (name, address, city and country).
+
+app.put("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+  const newName = req.body.name;
+  const newAddress = req.body.address;
+  const newCity = req.body.city;
+  const newCountry = req.body.country;
+
+  pool
+    .query(
+      "UPDATE customers SET name=$1, address=$2, city=$3, country=$4 WHERE id=$5",
+      [newName, newAddress, newCity, newCountry, customerId]
+    )
+    .then(() => res.send(`Customer ${customerId} updates!`))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+//- Add a new DELETE endpoint `/orders/:orderId` to delete an existing order along with all the associated order items
+
+app.delete("/orders/:orderId", function (req, res) {
+  const orderId = req.params.orderId;
+
+  pool
+    .query("DELETE FROM order_items WHERE order_id = $1", [orderId])
+    .then(() => pool.query("DELETE FROM orders WHERE id=$1", [orderId]))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
 app.listen(PORT, function () {
   console.log(`listening on ${PORT}`);
 });
