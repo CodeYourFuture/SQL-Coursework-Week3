20c8763ffde0fdbf890bd0eab6bc1012e3d1f81b
SaadiaELF <saadiaelfekak@gmail.com>
2023-02-02 23:27:27 +0000

    added last week project

---
+++ 2-api/cyf-ecommerce-api/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: "",
+  port: 5432,
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
+
+// Get all customers
+app.get("/customers", async function (req, res) {
+  try {
+    let result = await pool.query("SELECT * FROM customers");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// Get all suppliers
+app.get("/suppliers", async function (req, res) {
+  try {
+    let result = await pool.query("SELECT * FROM suppliers");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// Get all products
+app.get("/products", async function (req, res) {
+  try {
+    let result = await pool.query(
+      "SELECT p.product_name , pa.unit_price , s.supplier_name FROM products p INNER JOIN product_availability pa ON (p.id = pa.prod_id) INNER JOIN suppliers s ON (s.id = pa.supp_id)"
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
