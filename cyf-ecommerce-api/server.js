63cf8dbe7dacef1b2f2604bc9c2ac6293f8fbf3a
jaspreet kaur <kaurjas1928@yahoo.com>
2023-02-04 14:15:27 +0000

    added new endpoint to customers

--- cyf-ecommerce-api/server.js
+++ cyf-ecommerce-api/server.js
@@ -52,6 +52,15 @@ app.get("/products", (req, res) => {
     });
 });
 
+app.get("/customers/:customerId", (req, res) => {
+  const customerId = req.params.customerId;
+
+  pool.query("SELECT * FROM customers WHERE customers.id=$1", [customerId]).then((result) => res.json(result.rows)).catch((error)=> {
+    console.error(error);
+    res.status(500).json(error);
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
 });
