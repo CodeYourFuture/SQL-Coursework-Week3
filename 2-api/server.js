c5bfeed666a3e6baa420044a48b26ae6a23bb462
4maziar <53979524+8maziar@users.noreply.github.com>
2023-02-15 17:39:48 +0000

    post request done

--- 2-api/server.js
+++ 2-api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const { Pool } = require("pg");
 
+app.use(express.json());
+
 const db = new Pool({
   user: "nabwa",
   host: "localhost",
@@ -23,19 +25,25 @@ app.get("/customers/:id", function (req, res) {
   });
 });
 
+// POST METHOD
+app.post("/customers", function (req, res) {
+  console.log(req.body);
+  const { name, address, city, country } = req.body;
+
+  db.query("INSERT INTO customers (name, address, city, country) VALUES ($1, $2, $3, $4) RETURNING *", [name, address, city, country], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(201).send(`User added with ID: ${results.rows[0].id}`);
+  });
+});
+
+//
 app.get("/suppliers", function (req, res) {
   db.query("SELECT * FROM suppliers", (error, result) => {
     res.json(result.rows);
   });
 });
-
-// app.get("/products?name=Cup", (req, res) => {
-//   const searchKey = req.query.name;
-//   console.log(req.body);
-//   db.query("select product_name from products where product_name like '%$1%'", [searchKey], (err, result) => {
-//     res.json(result.rows);
-//   });
-// });
 
 app.get("/products", (req, res) => {
   const searchKey = req.query.name;
@@ -54,21 +62,7 @@ app.get("/products", (req, res) => {
       console.error(err);
       res.status(500).json(err);
     });
-
-  // db.query(
-  //   "select p.product_name, p_a.unit_price,s.supplier_name from products p join product_availability p_a on (p.id = p_a.prod_id) join suppliers s on (s.id = p_a.supp_id)",
-  //   [...params],
-  //   (error, result) => {
-  //     res.json(result.rows);
-  //   }
-  // );
 });
-
-// app.get("/products", (req, res) => {
-//   db.query("select p.product_name, p_a.unit_price,s.supplier_name from products p join product_availability p_a on (p.id = p_a.prod_id) join suppliers s on (s.id = p_a.supp_id)", (err, result) => {
-//     res.json(result.rows);
-//   });
-// });
 
 app.listen(8080, function () {
   console.log("Server is listening on port 8080.");
