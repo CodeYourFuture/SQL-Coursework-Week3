7830edfa6d8905dded6a7d5af72e73c8b96e790b
4maziar <53979524+8maziar@users.noreply.github.com>
2023-02-16 10:14:33 +0000

    post method availability done

--- 2-api/server.js
+++ 2-api/server.js
@@ -25,7 +25,7 @@ app.get("/customers/:id", function (req, res) {
   });
 });
 
-// POST METHOD
+// POST METHOD customers
 app.post("/customers", function (req, res) {
   console.log(req.body);
   const { name, address, city, country } = req.body;
@@ -38,6 +38,7 @@ app.post("/customers", function (req, res) {
   });
 });
 
+// POST METHOD products
 app.post("/products", function (req, res) {
   const { name } = req.body;
 
@@ -46,6 +47,33 @@ app.post("/products", function (req, res) {
       throw error;
     }
     res.status(201).send(`User added with ID: ${results.rows[0].id}`);
+  });
+});
+
+// post method product availability
+app.post("/availability", async (req, res) => {
+  const { price, supId, prodId } = req.body;
+  const query = "INSERT INTO product_availability (unit_price, supp_id, prod_id) VALUES ($1, $2, $3) RETURNING *";
+  const proIdCheck = await db.query("SELECT * FROM products WHERE id = $1", [prodId]).then((data) => data.rowCount > 0);
+  const supIdCheck = await db.query("SELECT * FROM suppliers WHERE id = $1", [supId]).then((data) => data.rowCount > 0);
+  const duplicateCheck = await db.query("SELECT * FROM product_availability WHERE prod_id = $1 and supp_id = $2", [prodId, supId]).then((data) => data.rowCount === 0);
+  if (proIdCheck && supIdCheck && Number.isInteger(price) && price > 0 && duplicateCheck) {
+    const result = await db.query(query, [price, supId, prodId]);
+    res.status(201).json(result.rows);
+  } else {
+    res.status(400).send("missing info or duplicated row");
+  }
+});
+
+app.put("/customers/:customerId", function (req, res) {
+  const customerID = +eq.params.id;
+  const { name, address, city, country } = req.body;
+
+  db.query("UPDATE customers SET name = $1, address = $2, city = $3, country = $4 WHERE id = $5", [name, address, city, country, customerID], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(202).send(`Customer modified with ID: ${id}`);
   });
 });
 
