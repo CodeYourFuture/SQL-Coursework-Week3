573c69c315a6eeb96bf55dff16927fec0127ad05
Miladepour <epour.milad@gmail.com>
2023-02-18 15:21:24 +0000

    Create basic server

---
+++ 2-api/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const { Pool } = require("pg");
+
+app.get("/", (req, res) => {
+  res.send("Database Project");
+});
+
+const db = new Pool({
+  user: "miladebrahimpour", // replace with you username
+  host: "localhost",
+  database: "cyf_ecommerce",
+  password: process.env.USER_PASS,
+  port: 5432,
+});
+
+app.get("/customers", function (req, res) {
+  db.query("SELECT * FROM customers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.get("/suppliers", function (req, res) {
+  db.query("SELECT * FROM suppliers", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.listen(5000, function () {
+  console.log("Server is listening on port 5000. Ready to accept requests!");
+});
